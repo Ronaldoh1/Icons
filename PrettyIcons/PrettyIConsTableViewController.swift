507c533c8ed71d98d514d56f37bcb1617c269933
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-08-21 20:19:34 -0500

    added tableViweController

---
+++ PrettyIcons/PrettyIConsTableViewController.swift
@@ -0,0 +1,99 @@
+//
+//  PrettyIConsTableViewController.swift
+//  PrettyIcons
+//
+//  Created by Ronald Hernandez on 8/21/16.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class PrettyIConsTableViewController: UITableViewController {
+    
+    var icons = [Icon]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let iconSets = IconSet.iconSets()
+        let iconSet = iconSets[0]
+        icons = iconSet.icons
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return icons.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("iconCell", forIndexPath: indexPath)
+        let icon = icons[indexPath.row]
+        
+        cell.textLabel?.text = icon.title
+        cell.detailTextLabel?.text = icon.subtitle
+        
+        if let image = icon.image {
+            cell.imageView?.image = image
+        }
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PrettyIcons/ViewController.swift
+++ PrettyIcons/ViewController.swift
@@ -9,17 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var icons = [Icon]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    let iconSets = IconSet.iconSets()
+    let iconSet = iconSets[0]
+    icons = iconSet.icons
+    automaticallyAdjustsScrollViewInsets = false
+    
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return icons.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("iconCell", forIndexPath: indexPath)
+        let icon = icons[indexPath.row]
+        
+        cell.textLabel?.text = icon.title
+        cell.detailTextLabel?.text = icon.subtitle
+        
+        if let iconImage = icon.image {
+            cell.imageView?.image = iconImage
+        }
+        return cell
+    }
+    
+}
