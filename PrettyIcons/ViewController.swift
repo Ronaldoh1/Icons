1f928637d137e41be95eb17a2ffdb0b1adf0708e
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-08-21 21:19:14 -0500

    added multple sections

--- PrettyIcons/ViewController.swift
+++ PrettyIcons/ViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var icons = [Icon]()
+    var iconSets = [IconSet]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    let iconSets = IconSet.iconSets()
-    let iconSet = iconSets[0]
-    icons = iconSet.icons
+     iconSets = IconSet.iconSets()
+    
     automaticallyAdjustsScrollViewInsets = false
     
   }
@@ -29,16 +28,18 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return icons.count
+        let iconSet = iconSets[section]
+        return iconSet.icons.count
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return iconSets.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("iconCell", forIndexPath: indexPath)
-        let icon = icons[indexPath.row]
+        let iconSet = iconSets[indexPath.section]
+        let icon = iconSet.icons[indexPath.row]
         
         cell.textLabel?.text = icon.title
         cell.detailTextLabel?.text = icon.subtitle
@@ -48,5 +49,15 @@ extension ViewController: UITableViewDataSource {
         }
         return cell
     }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let iconSet = iconSets[section]
+        return iconSet.name
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
     
 }
